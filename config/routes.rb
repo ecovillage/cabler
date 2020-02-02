c388f5832121b0dee128ac28431a6ce5fdca38aa
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-02-02 10:26:34 +0100

    resource route devices, links, locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'devices#index'
+  resources :devices do
+    resources :links, controller: 'devices/links'
+  end
+  resources :locations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
