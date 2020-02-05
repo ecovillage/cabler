567cf01a1fecc25f4792a4d90a359ef098c87dc8
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-02-05 07:01:06 +0100

    ease creation of devices at location

--- app/models/device.rb
+++ app/models/device.rb
@@ -5,7 +5,16 @@ class Device < ApplicationRecord
   has_many :link_other_ends, as: :other_end, class_name: 'Link', inverse_of: :other_end
   
 
-  validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+  # only if not empty
+  validates :name, presence: true, length: { minimum: 2 }#, uniqueness: true
+
+  validate :identifiable
+
+  def identifiable
+    if !(name.present? || model.present? || (model.present? || kind.present? && location.present?))
+      errors.add(:name, "need something to identify this device (name, model or kind + location)")
+    end
+  end
 
   def links
     Link.where(one_end: self).or(Link.where(other_end: self))
