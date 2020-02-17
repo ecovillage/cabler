191dc4a21792b8a197ae8142985ca7bd21064940
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-02-17 16:05:39 +0100

    extract Graph model from topology controller

--- app/controllers/topology_controller.rb
+++ app/controllers/topology_controller.rb
@@ -7,60 +7,13 @@ class TopologyController < ApplicationController
 
   # GET /dashboard
   def show
-    g = GraphViz.new(:G, type: :digraph)
-    #g = GraphViz.new(:G, type: :digraph, use: "fdp")
-    #g["rankdir"] = "LR"
-
-    @devices = Device.all
-    @locations = Location.all
-    @links = Link.all
-
-    device_nodes = {}
-    location_nodes = {}
-
-    nodes = {}
-
-    @devices.each do |device|
-      device_node = g.add_nodes(device.human_identifier)
-      device_node[:shape] = 'box3d'
-      device_node[:shape]='record'
-      device_node[:label] = "{#{device.human_identifier}|<p0> 1|<p1> 2|<p2> 3|<p3> 4|<p4> 5|<p5> 6}"
-      device_nodes[device] = device_node
-      nodes[device] = device_node
-    end
-
-    @locations.each do |location|
-      location_node = g.add_nodes(location.human_identifier)
-      location_node[:shape]='ellipse'
-      location_nodes[location] = location_node
-      nodes[location] = location_node
-    end
-
-
-    @devices.each do |device|
-      if device.location
-        edge = g.add_edges(device_nodes[device], location_nodes[device.location])
-        #edge[:label] = "@"
-      end
-      #connected_device = ConnectedDevice.new(device)
-    end
-
-    @links.each do |link|
-      if link.one_end && link.other_end
-        edge = g.add_edges(
-          {nodes[link.one_end] => "p#{link.slot_one_end}"},
-          {nodes[link.other_end] => "p#{link.slot_other_end}"}
-        )
-        edge[:label] = link.name
-      end
-      #g.add_edges
-    end
+    g = Graph.new
 
     respond_to do |format|
-      format.html { @png = g.output png: String }
-      format.png { render plain: g.output(png: String) }
-      format.svg { render plain: g.output(svg: String) }
-      format.dot { render plain: g.output(dot: String) }
+      format.html { @png = g.to_png }
+      format.png { render plain: g.to_png }
+      format.svg { render plain: g.to_svg }
+      format.dot { render plain: g.to_dot }
       #TODO better: send_data , send_file for .dot
     end
   end
---
+++ app/models/graph.rb
@@ -0,0 +1,85 @@
+# SPDX-FileCopyrightText: 2020 Felix Wolfsteller
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class Graph
+  def initialize devices: nil, locations: nil, group_locations: true
+    @g = GraphViz.new(:G, type: :graph, use: "dot")
+    #g = GraphViz.new(:G, type: :digraph, use: "fdp")
+
+    @g["rankdir"] = "LR"
+
+    @g["nodesep"] = "2"
+
+    @devices = devices || Device.all
+    @locations = locations || Location.all
+    @links = Link.all
+
+    device_nodes = {}
+    location_nodes = {}
+
+    nodes = {}
+
+    @locations.each do |location|
+      location_node = @g.add_nodes(location.human_identifier)
+      location_node[:shape]='ellipse'
+      location_node[:href]='ellipse'
+      location_nodes[location] = location_node
+      nodes[location] = location_node
+    end
+
+    @devices.each do |device|
+      device_node = @g.add_nodes(device.human_identifier)
+      #device_node[:shape] = 'box3d'
+      device_node[:shape]='record'
+      device_node[:label] = "{#{device.human_identifier}|<p0> 1|<p1> 2|<p2> 3|<p3> 4|<p4> 5|<p5> 6}"
+      device_nodes[device] = device_node
+      nodes[device] = device_node
+    end
+
+
+    @devices.each do |device|
+      if device.location
+        edge = @g.add_edges(device_nodes[device], location_nodes[device.location]) #, label: '<<br>html</br>label!>'
+        edge[:arrowhead] = "none"
+        #edge[:label] = "@"
+        #edge[:href] = "@"
+      end
+      #connected_device = ConnectedDevice.new(device)
+    end
+
+    @links.each do |link|
+      if link.one_end && link.other_end
+        edge = @g.add_edges(
+          {nodes[link.one_end] => "p#{link.slot_one_end}"},
+          {nodes[link.other_end] => "p#{link.slot_other_end}"}
+        )
+        edge[:label] = link.name
+        edge[:arrowhead] = "normal"
+        edge[:arrowtail] = "normal"
+        edge[:dir] = "both"
+      end
+      #g.add_edges
+    end
+  end
+
+  def to_svg
+    @g.output svg: String
+  end
+
+  def to_png
+    @g.output png: String 
+  end
+
+  def to_dot
+    @g.output canon: String
+  end
+
+  private
+
+  def add_node
+  end
+
+  def add_edge
+  end
+end
