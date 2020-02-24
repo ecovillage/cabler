fe116a1cd5e9508b9e670cbcf4a8cf8e8f9e90b1
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-02-24 12:02:10 +0100

    0.1.1

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Cabler
   class Application < Rails::Application
-    VERSION = "0.1.0".freeze
+    VERSION = "0.1.1".freeze
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
