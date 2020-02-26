61b61dfa9ac0731464d6a4b54874613d3b8473f7
syler <hileo77@gmail.com>
2020-02-26 19:16:12 +0100

    change method names to snake_case

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,20 @@
 module ApplicationHelper
 
-  def getNavItemClass(path)
-    return "navbar-item #{addActiveClass(path)}"
+  def nav_item_class(path)
+    return "navbar-item #{active_class(path)}"
   end
 
   private
 
-    def addActiveClass(path)
+    def active_class(path)
       return 'is-active' if controller_name == path.remove('/')
     end
 
     def log(text)
-      logger.debug(getColoredString(text, {'r' => '255', 'g' => '20', 'b' => '90'}))
+      logger.debug(colored_string(text, {'r' => '255', 'g' => '20', 'b' => '90'}))
     end
 
-    def getColoredString(str, color)
+    def colored_string(str, color)
       return "\u001b[38;2;#{color['r']};#{color['g']};#{color['b']}m#{str}\u001b[0m"
     end
 end
