b5cfe564cb6fdf4213e5f70ab2bc6dc7096e289c
syler <hileo77@gmail.com>
2020-02-26 19:22:41 +0100

    remove duplicate method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,10 @@
 module ApplicationHelper
 
-  def nav_item_class(path)
-    return "navbar-item #{active_class(path)}"
-  end
+  # def log(text)
+  #   logger.debug(colored_string(text, {'r' => '255', 'g' => '20', 'b' => '90'}))
+  # end
 
-  private
-
-    def active_class(path)
-      return 'is-active' if controller_name == path.remove('/')
-    end
-
-    def log(text)
-      logger.debug(colored_string(text, {'r' => '255', 'g' => '20', 'b' => '90'}))
-    end
-
-    def colored_string(str, color)
-      return "\u001b[38;2;#{color['r']};#{color['g']};#{color['b']}m#{str}\u001b[0m"
-    end
+  # def colored_string(str, color)
+  #   return "\u001b[38;2;#{color['r']};#{color['g']};#{color['b']}m#{str}\u001b[0m"
+  # end
 end
