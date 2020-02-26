bd0dbeb2119e1cbc350be51713ce213e126a567c
syler <hileo77@gmail.com>
2020-02-26 19:46:37 +0100

    add settings page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   root 'dashboard#show'
   resources :devices do
     resources :links, controller: 'devices/links'
