e4010212c704081da752f5d34cdd849c88d0d7ae
syler <hileo77@gmail.com>
2020-02-28 18:30:22 +0100

    resolve #7 (for real this time)

---
+++ db/migrate/20200228171956_change_links_column_names.rb
@@ -0,0 +1,8 @@
+class FixSlotColumnName < ActiveRecord::Migration[6.0]
+  def change
+    change_table :links do |t|
+      t.rename :slot_one_end, :port_one_end
+      t.rename :slot_other_end, :port_other_end
+    end
+  end
+end
