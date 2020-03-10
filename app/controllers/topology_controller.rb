a46bcf32a185eee3a22b8055612bc277c53426ac
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-10 09:40:48 +0100

    (#31) add pan and zoom to topology svg (breaks)

--- app/controllers/topology_controller.rb
+++ app/controllers/topology_controller.rb
@@ -8,16 +8,16 @@ class TopologyController < ApplicationController
   # GET /dashboard
   def show
     @graph_configuration = GraphConfiguration.new(graph_configuration_params)
-    g = Graph.new rankdir: @graph_configuration.rank_dir,
+    @graph = Graph.new rankdir: @graph_configuration.rank_dir,
       splines:       @graph_configuration.splines,
       box_locations: @graph_configuration.box_locations,
       show_ports:    @graph_configuration.show_ports.to_sym
 
     respond_to do |format|
-      format.html { @png = g.to_png }
-      format.png { render plain: g.to_png } # send_data , type: 'image/png', disposition: 'inline'
-      format.svg { render plain: g.to_svg }
-      format.dot { send_data g.to_dot,
+      format.html { @png = @graph.to_png }
+      format.png { render plain: @graph.to_png } # send_data , type: 'image/png', disposition: 'inline'
+      format.svg { render plain: @graph.to_svg }
+      format.dot { send_data @graph.to_dot,
                      filename: 'cabler_graph.dot',
                      type: 'text/plain',
                      disposition: 'attachment'
