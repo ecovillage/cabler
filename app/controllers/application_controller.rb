a3323f527ef2ec9be60e01cfca080ca8c748a042
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-09-27 16:41:32 +0200

    set nav_locations in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 class ApplicationController < ActionController::Base
+  before_action :set_locations_for_nav, unless: :devise_controller?
+
+  def set_locations_for_nav
+    @nav_locations = Location.all.order(:name).where(parent: nil)
+  end
 end
